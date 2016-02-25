f60c351bd20ea18c0ba9aee2fd769e97004db0a3
Roppin.L <robin.ljungqvist@gmail.com>
2016-02-25 11:38:10 +0100

    Added Exercises 8, 9 and 10.

---
+++ Exercises/Exercise 10/Exercise 10/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Car : Vehicle
+    {
+        private bool engineIsRunning = false;
+        public Car(string vehicleType): base (vehicleType)
+        {
+            this._VehicleType = vehicleType;
+        }
+        public void StartEngine()
+        {
+            engineIsRunning = true;
+        }
+        public void StopEngine()
+        {
+            engineIsRunning = false;
+        }
+    }
+}
---
+++ Exercises/Exercise 10/Exercise 10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleData VD = new VehicleData();
+            Car Volvo = new Car("Bil: Volvo");
+            Boat Segelbåt = new Boat("Båt: Motordriven segelbåt", 3);
+            VD.PrintVehicle(Volvo);
+            VD.PrintVehicle(Segelbåt);
+            Console.ReadKey();
+        }
+    }
+}
