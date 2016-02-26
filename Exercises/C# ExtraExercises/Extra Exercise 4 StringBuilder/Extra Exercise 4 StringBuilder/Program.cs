c006e6732483baa042d7aa11612d175016e66c64
Roppin.L <robin.ljungqvist@gmail.com>
2016-02-26 15:32:35 +0100

    Extra exercise 4 String Builder

---
+++ Exercises/C# ExtraExercises/Extra Exercise 4 StringBuilder/Extra Exercise 4 StringBuilder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_Exercise_4_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("< table >");
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sb.AppendLine("     <tr bgcolor=\"#CCCCCC\">");
+                }
+                else
+                {
+                    sb.AppendLine("     </tr>");
+                }
+                sb.AppendLine(string.Format("         <td>Row{0}</td>", i));
+                for (int j = 0; j < 9; j++)
+                {
+                    sb.AppendLine("         <td>xxxxx<td>");
+                }
+                sb.AppendLine("     </tr>");
+            }
+            sb.AppendLine("< /table >");
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+    }
+}
