a2771f72dc32a1d24a3ecf1025d631cf273575de
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-01 15:10:49 +0100

    Exercise 14

---
+++ Exercises/Exercise 14/Exercise 14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var TBC = new TableCell("Hallå ja!");
+            var messageNamn = new TableCell("Mitt namn är Robin.");
+            var messageSkola = new TableCell("Jag går på EC utbildning i helsingborg");
+
+            var TBR = new TableRow(TBC);
+            TBR.AddCell(messageNamn);
+            TBR.AddCell(messageSkola);
+
+            var TBR1 = new TableRow(TBC);
+            var TBR2 = new TableRow(TBC);
+            var TBR3 = new TableRow(TBC);
+            var Tab = new Table(TBR);
+            Tab.AddRow(TBR1);
+            Tab.AddRow(TBR2);
+            Tab.AddRow(TBR3);
+            var message = Tab.ToString();
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercises/Exercise 14/Exercise 14/TableRow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14
+{
+    class TableRow
+    {
+        private List<TableCell> _tableCellList;
+
+        public int _Count
+        {
+            get
+            {
+                return _tableCellList.Count;
+            }
+        }
+
+        public TableRow(TableCell tableCell)
+        {
+            _tableCellList = new List<TableCell>();
+            _tableCellList.Add(tableCell);
+        }
+        public void AddCell(TableCell cell)
+        {
+            _tableCellList.Add(cell);
+        }
+        public string GetAllCells()
+        {
+            var allCells = string.Empty;
+            foreach (var Cell in _tableCellList)
+            {
+                allCells += Cell.ToString();
+            }
+            return allCells;
+        }
+
+        public override string ToString()
+        {
+            return $"<tr>{GetAllCells()}</tr>";
+
+        }
+
+    }
+}
