22ed7f1a6895bc1cb9a01458b8ea0269c7b4f463
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-14 13:39:18 +0100

    Exercise 18

---
+++ Exercises/Excercise 18/Excercise9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrontEnd UI = new FrontEnd();
+            UI.Menu();
+        }
+    }
+}
