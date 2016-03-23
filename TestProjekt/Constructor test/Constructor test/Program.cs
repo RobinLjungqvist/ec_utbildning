b96833add529b0a1ea0dd17fd7e949a7d0d7044d
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-23 13:34:24 +0100

    Constructor test

---
+++ TestProjekt/Constructor test/Constructor test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataList = new List<Test>();
+
+            var one = new Test(1, 1);
+            var two = new Test(2,2);
+
+            dataList.Add(one);
+            dataList.Add(two);
+
+            var thing = new Thingy(dataList);
+
+            thing.ChangeAValue();
+
+            Console.WriteLine("MasterList: Changed value:");
+            Console.WriteLine(dataList[0].data1);
+            Console.WriteLine("Copiedlist:");
+            Console.WriteLine(thing.GetList(0).data1);
+
+            double ost = 5.2;
+
+            int ostboll = Convert.ToInt32(ost);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestProjekt/Constructor test/Constructor test/Thingy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_test
+{
+    class Thingy
+    {
+        
+        List<Test> dataTest;
+
+        public Thingy( List<Test> dataList)
+        {
+            
+            dataTest = new List<Test>(dataList);
+            dataList.ForEach(x => dataTest.Add(new Test(x.data1, x.data2)));
+        }
+
+        public Test GetList(int index)
+        {
+            return dataTest[index];
+        }
+        public void ChangeAValue()
+        {
+            dataTest[0].data1 = 5; 
+        }
+    }
+}
