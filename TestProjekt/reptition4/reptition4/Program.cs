cf1d7a2f578e7893fdf35156df96e1c29c2cd56d
Roppin.L <robin.ljungqvist@gmail.com>
2016-03-29 13:50:45 +0200

    repitition 3 och 4

---
+++ TestProjekt/reptition4/reptition4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reptition4
+{
+    class Program
+    {
+        public static bool IsPrime(int input)
+        {
+            if (input == 1)
+                return false;
+            if (input == 2)
+                return true;
+            if ((input % 2 == 0))
+                return false;
+
+            for(int i = 3;  i < input; i+=2)
+            {
+                if ((input % i) == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if(IsPrime(i))
+                {
+                    Console.Write(i + "  ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
