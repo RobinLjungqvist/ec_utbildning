5cc80f6371413ca17d5556bbd9bc504427a2eaf7
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-04-21 17:56:14 +0200

    Övning6

---
+++ ToresExtraUppgifter/Övning6/Övning6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> myList = new List<User>();
+            User Robin = new User();
+            Robin.Name = "Robin";
+            Robin.Age = 25;
+            Robin.Country = "Sweden";
+            Robin.City = "Helsingborg";
+            myList.Add(Robin);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter a search term: ");
+                var search = Console.ReadLine();
+                var result = searchUser(myList, search);
+                result.ForEach(s => Console.WriteLine($"Name: {s.Name} Age: {s.Age} City: {s.City} Country: {s.Country} "));
+                Console.ReadKey();
+                
+            }
+        }
+
+        public static List<User> searchUser(List<User> list, string SearchInput)
+        {
+            var tempList = new List<User>();
+
+            foreach (var person in list)
+            {
+                if (person.Name.ToLower() == SearchInput.ToLower())
+                {
+                    tempList.Add(person);
+                }
+                else if (person.Age.ToString() == SearchInput)
+                {
+                    tempList.Add(person);
+                }
+                else if (person.City.ToLower() == SearchInput.ToLower())
+                {
+                    tempList.Add(person);
+                }
+                else if (person.Country.ToLower() == SearchInput.ToLower())
+                {
+                    tempList.Add(person);
+                }
+            }
+            return tempList;
+        }
+    }
+}
---
+++ ToresExtraUppgifter/Övning6/Övning6/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning6
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; } 
+    }
+}
