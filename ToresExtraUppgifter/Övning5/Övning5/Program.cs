7d1b5a7a01e173f8d5e73685b0bab322ba90d733
RobinLjungqvist <robin.ljungqvist@gmail.com>
2016-04-21 17:35:49 +0200

    Övning5

---
+++ ToresExtraUppgifter/Övning5/Övning5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write something in swedish.");
+            var userInput = Console.ReadLine();
+            Console.WriteLine("This text is now good for english systems.");
+            Console.WriteLine(EnglishText(userInput));
+            Console.ReadKey();
+        }
+
+        public static string EnglishText(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in input)
+            {
+                switch(letter)
+                {
+                    case ('å'):
+                        sb.Append("aa");
+                        break;
+                    case ('Å'):
+                        sb.Append("Aa");
+                        break;
+                    case 'ä':
+                        sb.Append("ae");
+                        break;
+                    case 'Ä':
+                        sb.Append("Ae");
+                        break;
+                    case 'ö':
+                        sb.Append("oo");
+                        break;
+                    case 'Ö':
+                        sb.Append("Oo");
+                        break;
+                    default:
+                        sb.Append(letter);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
